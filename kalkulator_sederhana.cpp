db07e9300bf3c935b44a229f46c99cb8c42a2db9
ALVIAN_085 <114425441+ALVIANRML@users.noreply.github.com>
2023-04-03 21:48:29 +0700

    Update kalkulator_sederhana.cpp

--- kalkulator_sederhana.cpp
+++ kalkulator_sederhana.cpp
@@ -5,44 +5,57 @@ using namespace std;
 
 int main(){
     float a,b;
+    char aritmatika;
 
 
 
 
-    cout << "         KALKULATOR SEDERHANA          /n";
+    cout << "         KALKULATOR SEDERHANA          \n";
     cout << endl;
     cout << "Kalkulator ini akan menjumlahkan, mengurankan, mengali, membagi, dan juga menghitung modulos.";
     cout << endl;
 
     cout << "Masukkan angka pertama: ";
     cin  >> a;
-
+    
+    cout << "Mau diapain +,-,*/,%(modulos): ";
+    cin  >> aritmatika;
+    
     cout << "Masukkan angka kedua: ";
     cin  >> b;
-
+    
     //penjumlahan
+    if (aritmatika == '+')
+        { 
     cout << "hasil penjumlahan    " << a << "+" << b << " = ";
     cout << a+b << endl;
-
+        }
     //pengurangan
-    cout << "hasil pengurangan    " << a << "-" << b << " = ";
-    cout << a-b << endl;
-
+    else if (aritmatika == '-')
+        {
+        cout << "hasil pengurangan    " << a << "-" << b << " = ";
+        cout << a-b << endl;
+        }
     //perkalian
+    else if (aritmatika == '*')
+        {
     cout << "hasil perkalian      " << a << "*" << b << " = ";
     cout << a*b << endl;
-
-    //perkalia
+        }
+    //pembagian
+    else if (aritmatika == '/')
+        {
     cout << "hasil pembagian      " << a << "/" << b << " = ";
     cout << setprecision(0) << a/b << endl;
-
-
+        }
+    //modulos
+    else if (aritmatika == '%')
+        {
     int c = a;
     int d = b;
-    //modulos
     cout << "hasil modulos        " << c << " mod " << d << " = ";
     cout << c%d << endl;
-
+        }
     cin.get();
 return 0;
 }
